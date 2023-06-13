229f75344f89da3af7ad270e13708f07eaaaea77
Deepak Saini <deepaksainiofficial17@gmail.com>
2023-06-13 16:31:18 +0530

    New changes added

---
+++ Scripts/Data_Processing.py
@@ -0,0 +1,91 @@
+#required modules
+import numpy as np
+import pandas as pd
+from imblearn.over_sampling import RandomOverSampler
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+
+
+#function for handling imbalace dataset by oversampling method using RandomOverSampler of imblearn module 
+def data_balancer(X, y):
+    try:
+        over_sampler = RandomOverSampler(random_state = 42)
+        X_resampled, y_resampled = over_sampler.fit_resample(X, y)
+        return (X_resampled, y_resampled)
+    except:
+        print("Something else went wrong in data_balancer")
+
+
+#function for selction of features which is correlated mostly
+def feature_selector(dataset, threshold):
+    try:
+        col_corr = set()  
+        corr_matrix = dataset.corr()
+        for i in range(len(corr_matrix.columns)):
+            for j in range(i):
+                if abs(corr_matrix.iloc[i, j]) > threshold:
+                    colname = corr_matrix.columns[i]  
+                    col_corr.add(colname)
+        return col_corr
+    except:
+        print("Something else went wrong in feature_selector")
+
+
+# function for scaling dataset columns into same scale or -1 to 1 
+def data_transformer(X, y):
+    try:
+        cols = list(X.columns)
+        scaler = StandardScaler()
+        scaler.fit(X)
+        X = scaler.transform(X)    
+        X = pd.DataFrame(X, columns=cols)
+        y = y.apply(lambda x: 1 if x<7 else 0)
+        return (X, y)
+    except:
+        print("Something else went wrong")
+
+#function for spliting datasets into train and test datasets
+def train_test_data(X,y):
+    try:
+        X_train , X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+        return (X_train , X_test, y_train, y_test)
+    except:
+        print("Something else went wrong")
+
+
+def data_dump(X, y, path):
+    try:
+        data = pd.concat([X, y], axis=1)
+        print('concetenation done!')
+        data.to_csv(path, index=False)
+    except:
+        print("Something else went wrong in data_dump")
+
+#loading dataset from data_source folder
+df = pd.read_csv("Data\Raw\winequality-red.csv")
+
+#deleting duplicate values from dataset
+df.drop_duplicates(inplace=True, ignore_index = True)
+
+#converting dataset into independent and dependent dataset
+X = df.drop(['quality'], axis=1)
+y = df['quality']
+
+#balancing imbalance data
+X, y = data_balancer(X, y)
+
+#extra columns which is not have much information
+corr_features = feature_selector(X, 0.7)
+X = X.drop(['citric acid'], axis=1)
+data_dump(X, y, 'Data\Features\engineered_features.csv')
+
+#scaling data into -1 to 1
+X, y = data_transformer(X, y) 
+data_dump(X, y, 'Data\Processed\preprocessed_data.csv')
+
+#spliting data into train and test datasets
+X_train, X_test, y_train, y_test = train_test_data(X, y)
+data_dump(X_train, y_train, r'Data\Splits\train.csv')
+data_dump(X_test, y_test, r'Data\Splits\test.csv')
---
+++ Scripts/Model_Training.py
@@ -0,0 +1,110 @@
+import Data_Processing
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+from sklearn.metrics import r2_score, accuracy_score
+import pickle
+
+def train_test():
+    train = pd.read_csv(r"Data\Splits\train.csv")
+    test = pd.read_csv(r"Data\Splits\test.csv")
+
+    X_train = train.drop(['quality'], axis=1)
+    y_train = train['quality']
+    X_test = test.drop(['quality'], axis=1)
+    y_test = test['quality']
+
+    return (X_train, X_test, y_train, y_test)
+
+
+def models_report(X_train, X_test, y_train, y_test):
+    accuracy_report = {}
+    params_report = {}
+
+    models = {
+        "Logistic Classifier":LogisticRegression(), 
+        "Random Forest Classifier":RandomForestClassifier(), 
+        "Support Vector Classifier":SVC(), 
+        "K Neighbors Classifier":KNeighborsClassifier(), 
+        "XGB Classifier":XGBClassifier()
+    }
+    params = {
+        "Logistic Classifier":{
+            'tol':[1e-2, 1e-3, 1e-4, 1e-5],
+            'C':[0.5, 0.75, 1, 1.5, 2],
+            'solver':['lbfgs', 'liblinear', 'newton-cg', 'newton-cholesky', 'sag', 'saga'],
+        },
+        "Random Forest Classifier":{
+            'n_estimators':[90, 100, 110, 120],
+            'criterion':['gini', 'entropy', 'log_loss'],
+            'max_features':['sqrt', 'log2', None]
+        },
+        "Support Vector Classifier":{
+            'C':[1,1.5,2,2.5,3],
+            'gamma':['scale', 'auto'],
+        },
+        "K Neighbors Classifier":{
+            'n_neighbors':[4,5,6,7],
+            'weights':['uniform', 'distance'],
+            'p':[1,2],
+        },
+        "XGB Classifier":{
+            'n_estimators':[2,3,4,5],
+            'learning_rate':[0.5,1,1.5,2],
+        }
+    }   
+
+    for i in range(len(list(models))):
+        model = list(models.values())[i]
+        param = params[list(models.keys())[i]]
+    
+        gs = GridSearchCV(model, param, cv=3)
+        gs.fit(X_train, y_train)
+    
+        model.set_params(**gs.best_params_)
+        model.fit(X_train, y_train)
+    
+        y_train_pred = model.predict(X_train)
+        y_test_pred = model.predict(X_test)
+    
+        train_model_score = accuracy_score(y_train, y_train_pred)
+        test_model_score = accuracy_score(y_test, y_test_pred)
+    
+        accuracy_report[list(models.keys())[i]] = [test_model_score]
+        params_report[list(models.keys())[i]] = gs.best_params_
+
+
+    return (models, accuracy_report, params_report)
+
+def model_trainer(models, accuracy_report, params_report):
+    best_accuracy = max(sorted(accuracy_report.values()))
+    best_model_name = list(accuracy_report.keys())[
+        list(accuracy_report.values()).index(best_accuracy)
+    ]   
+    best_model = models[best_model_name]
+    params = params_report[best_model_name]
+    model = best_model
+    model.set_params(**params)
+
+    model.fit(X_train, y_train)
+
+    with open('Models\Saved_Models\model.pkl', 'wb') as file:
+        pickle.dump(model, file)
+
+    print("Model trained successfully with parameters {}".format(params))
+
+
+
+X_train, X_test, y_train, y_test = train_test()
+
+models, accuracy_report, params_report = models_report(X_train, X_test, y_train, y_test)
+
+model_trainer(models, accuracy_report, params_report)
